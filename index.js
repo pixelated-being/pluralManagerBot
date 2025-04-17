a12a48d424a6321fe3ed177a045b54b306b06e07
pixelated-being <ajd0929@gmail.com>
2025-04-17 12:55:40 -0400

    once again, testing. sigh...

--- index.js
+++ index.js
@@ -27,10 +27,6 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.on(Events.InteractionCreate, interaction => {
-	console.log(interaction);
-});
-
 
 
 // When the client is ready, run this code (only once).
@@ -38,6 +34,28 @@ client.on(Events.InteractionCreate, interaction => {
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
 });
 
 // Log in to Discord with your client's token
