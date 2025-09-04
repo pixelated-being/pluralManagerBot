e5666d384d341bc0a1b335bab6cab82305199874
pixelated-being <ajd0929@gmail.com>
2025-09-04 12:15:21 -0400

    temp removal of reorder.js to test stuff

--- events/reorder.js
+++ events/reorder.js
@@ -1,11 +1,26 @@
-const { Events } = require('discord.js');
+// const { Events } = require('discord.js');
 
-client.on(Events.messageCreate, async message => {
-        if(client.ignoreUsers.hasAll(interaction.user.id, interaction.guildId)) {
-            return;
-        }
-        else {
-            // placeholder for testing stuff, will update accordingly
-            await interaction.reply('channel will be moved!');
-        }
-});
+// module.exports = {
+// 	name: Events.messageCreate,
+// 	async execute(interaction) {
+// 		if (interaction.isChatInputCommand()) return;
+
+// 		const command = interaction.client.commands.get(interaction.commandName);
+
+// 		if (!command) {
+// 			console.error(`No command matching ${interaction.commandName} was found.`);
+// 			return;
+// 		}
+
+// 		try {
+// 			await command.execute(interaction);
+// 		} catch (error) {
+// 			console.error(error);
+// 			if (interaction.replied || interaction.deferred) {
+// 				await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+// 			} else {
+// 				await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+// 			}
+// 		}
+// 	},
+// };
