3e1c37f18a6ad160b998539c77d45927f6ffe53e
pixelated-being <ajd0929@gmail.com>
2025-09-09 08:12:13 -0400

    beginning of database additions to keep user prefs through resets

--- events/reorder.js
+++ events/reorder.js
@@ -3,20 +3,21 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(newMessage) {
-        console.log("Test ")
-	
         let message = newMessage.content;
-        console.log(message);
+        
         const temp = true; // temp variable in place of an eventual "include commands" setting (per-channel?)
         
         const MessageChannel = newMessage.channel; // You forgot to define MessageChannel
-        
         
         console.log("channel position is: " + MessageChannel.position);
 		if (MessageChannel.position == 0) {
 			console.log("channel not moved because already at the top");
 			return;
 		}
+        else if (false) { // this is eventually gonna include the user ignore database thing... eventually...
+            console.log("advancement made: how did we get here?");
+            return;
+        }
         else if (message.includes("pk;" || "pk ;" || "pk!" || "pk !") && temp) {
             console.log("message not counted because it's a pluralkit command!");
             return;
