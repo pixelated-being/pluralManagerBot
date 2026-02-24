2a78e87038bb07b5f8b6ff5f534f2a5ed3e1536a
pixelated-being <ajd0929@gmail.com>
2026-02-24 11:44:16 -0500

    okay i have literally no idea whats going wrong anymore. THERES NOTHING CHANGED THAT COULD BE THAT BROKEN

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-// const { Client } = require('pg');
-// require('dotenv').config();
+const { dbClient } = require('pg');
+require('dotenv').config();
 
-// const client = new Client({
-//   user: 'postgres',
-//   host: process.env.MACHINE_IP,
-//   database: 'postgres',
-//   password: process.env.DATABASE_PASSWORD,
-//   port: 5433
-// });
+const dbclient = new dbClient({
+  user: 'postgres',
+  host: process.env.MACHINE_IP,
+  database: 'postgres',
+  password: process.env.DATABASE_PASSWORD,
+  port: 5433
+});
 
-// client.connect()
-//   .then(() => console.log('Successfully connected to the bot database!'))
-//   .catch(err => console.error('Connection error', err.stack));
+dbclient.connect()
+  .then(() => console.log('Successfully connected to the bot database!'))
+  .catch(err => console.error('Connection error', err.stack));
